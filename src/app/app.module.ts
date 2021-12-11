4262fd103f5911553907b43b9baf49c91d8b251a
derdem7 <derdemm7@gmail.com>
2021-12-11 21:46:47 +0300

    sign in/up comps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './comp/login/login.component';
+import { SignUpComponent } from './comp/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule
